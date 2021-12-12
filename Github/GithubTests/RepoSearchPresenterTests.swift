396901fe532c1f3872b7e1d5b7ce697504b4370f
Emrah Akgül <emrah.akgul@vngrs.com>
2021-12-12 22:09:32 +0300

    add missing test cases

--- Github/GithubTests/RepoSearchPresenterTests.swift
+++ Github/GithubTests/RepoSearchPresenterTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 import GithubAPI
 @testable import Github
+import Accelerate
 
 let searchResponse: SearchReposContainerDTO = SearchReposContainerDTO(
     totalCount: 2, items:
@@ -35,7 +36,12 @@ let searchResponse: SearchReposContainerDTO = SearchReposContainerDTO(
         ]
 )
 
-class RepoSearchViewPresenterTests: XCTestCase {
+final class RepoSearchViewPresenterTests: XCTestCase {
+
+    enum RepoSearchError: Error {
+
+        case dummyError
+    }
 
     private var view: MockRepoSearchView!
     private var interactor: MockRepoSearchInteractor!
@@ -70,6 +76,44 @@ class RepoSearchViewPresenterTests: XCTestCase {
         sut.searchRepos(with: "tempText")
 
         XCTAssertEqual(interactor.invokedSearchParameters?.text, "tempText")
+    }
+
+    func test_givenRepos_whenSelectedRepo_thenInvokesRepositoryDetail() {
+        // Given
+        sut.handleSearchingRepos(result: .success(searchResponse))
+        // When
+        sut.selectRepo(at: 0)
+        XCTAssertTrue(router.invokedNavigateToRepositoryDetail)
+    }
+
+    func test_givenRepos_whenSelectedAvatar_thenInvokesUserDetail() {
+        // Given
+        sut.handleSearchingRepos(result: .success(searchResponse))
+        // When
+        sut.selectAvatar(at: 0)
+        XCTAssertTrue(router.invokedNavigateToUserDetail)
+    }
+
+    func test_givenRepos_whenDeleted_thenInvokesInteractorReset() {
+        // Given
+        sut.handleSearchingRepos(result: .success(searchResponse))
+        // When
+        sut.removeRepos()
+        XCTAssertTrue(interactor.invokedReset)
+    }
+
+    func test_whenPaginationHappened_thenNumberOfItemsIncreased() {
+        // When
+        sut.handleSearchingRepos(result: .success(searchResponse))
+        sut.handleSearchingRepos(result: .success(searchResponse))
+        let numberOfItems = (searchResponse.items?.count ?? .zero) * 2
+        XCTAssertEqual(sut.numberOfItems, numberOfItems)
+    }
+
+    func test_whenRepoRetrievalFailed_thenErrorHappens() {
+
+        sut.handleSearchingRepos(result: .failure(RepoSearchError.dummyError))
+        XCTAssertTrue(view.invokedShowError)
     }
 
     func test_numberOfItems_CellPresentationCountIsNil_ReturnZero() {
